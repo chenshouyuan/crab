2278833a9328a0f8b1608872cb4f50b1f23c1141
Shouyuan Chen <chenshouyuan@gmail.com>
2011-10-09 22:50:30 +0800

    added basic examples; fixed scikits.learn.base reference

---
+++ example.py
@@ -0,0 +1,30 @@
+from scikits.crab.models import MatrixPreferenceDataModel
+from scikits.crab.metrics.pairwise import cosine_distances
+from scikits.crab.similarities import ItemSimilarity
+from scikits.crab.recommenders.knn import ItemBasedRecommender
+
+movies = {'Marcel Caraciolo': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
+ 'The Night Listener': 3.0},
+'Luciana Nunes': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 3.5},
+'Leopoldo Pires': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+ 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Lorena Abreu': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+ 'The Night Listener': 4.5, 'Superman Returns': 4.0,
+ 'You, Me and Dupree': 2.5},
+'Steve Gates': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+ 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 2.0},
+'Sheldom': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+ 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Penny Frewman': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0},
+'Maria Gabriela': {}}
+
+model = MatrixPreferenceDataModel(movies)
+similarity = ItemSimilarity(model, cosine_distances, 5)
+recommender = ItemBasedRecommender(model, similarity)
+
+userid = 'Leopoldo Pires'
+print recommender.recommend(userid, 3)
